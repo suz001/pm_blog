5b57a22fbefcffbf89529cab687fe57f79a40a56
junshen000 <75180661+junshen000@users.noreply.github.com>
2023-05-24 15:53:27 -0400

    add contact page
    Without submit method, 05/24

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import '../css/Contact.css';
 function Contact() {
   return (
-    <div>
-        <Navbar/>
-        Contact
-        </div>
+    <div class='container'>
+      <Navbar/>
+      <form id='contact'>
+        
+        <h1>Contact <span>Here</span></h1>
+        <label for="name">Contact Name</label>
+        <input type="text" name="name" id="" required/>
+        <label for="email">Contact Email</label>
+        <input type="text" name="email" id="" required/>
+        <label for="phone">Phone</label>
+        <input type="text" name="phone" id=""required/>
+        <label for="message">Subject</label>
+        <textarea name="text" id="message" cols="30" rows="10" required/> 
+        <input type="submit" value='Submit'/>
+      </form>      
+    </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
   )
 }
 
