8e6f14807534af28e986bf8ccd3f35960ea810bb
junshen000 <75180661+junshen000@users.noreply.github.com>
2023-05-28 17:34:24 -0400

    05/28 contact add

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Navbar from '../components/Navbar';
 import '../css/Contact.css';
-function Contact() {
+
+const Contact = () =>{
+
+  const [data, setData]=useState({name:"",email:"",phone:"",message:""});
+
+  const handleChange = (e) =>{
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({...data,[name]:value})
+
+  }
+  const handleSubmit = async() =>{
+  }
+/*
+
+  const handleSubmit = async() =>{
+    let formField = new FormData();
+    formField.append('name',data.name);
+    formField.append('email',data.email);
+    formField.append('phone',data.phone);
+    formField.append('message',data.message);
+
+    await axios({
+      method:'post',
+      url: 'http://localhost:8000/contact/',
+      data: formField,
+    }).then((response)=>{
+
+    })
+
+  }
+  */
+
   return (
-    
+  
     <div>
       <Navbar/>
     <div class='container'>
-      <form id='contact'>
+      <form id='contact' onSubmit={handleSubmit}>
+        <label/>
         <h1>Contact <span>Here</span></h1>
-        <label for="name">Contact Name</label>
-        <input type="text" name="name" id="" required/>
-        <label for="email">Contact Email</label>
-        <input type="text" name="email" id="" required/>
-        <label for="phone">Phone</label>
-        <input type="text" name="phone" id=""required/>
-        <label for="message">Subject</label>
-        <textarea name="text" id="message" cols="30" rows="10" required/> 
+        <label/>
+        <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder="Enter Name" required/>
+        <input type="text" name="email" id="" onChange={handleChange} value={data.email} placeholder="Enter Email" required/>
+        <input type="text" name="phone" id="" onChange={handleChange} value={data.phone} placeholder="Enter Phone" required/>
+        <textarea name="message" id="message" cols="30" rows="10" onChange={handleChange} value={data.message} required> Enter text here... </textarea>
         <input type="submit" value='Submit'/>
       </form>      
-    </div>     
+    </div>    
     </div>   
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
   )
